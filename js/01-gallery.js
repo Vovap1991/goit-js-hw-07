0d649a3006da2818bbde6a189e0e9a33912183f8
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-06-10 21:56:03 +0300

    task 1, part 1

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+  })
+  .join("");
+
+galleryContainerEl.insertAdjacentHTML("beforeend", markup);
+
+galleryContainerEl.addEventListener("click", onClickHandler);
+
+function onClickHandler(event) {
+  event.preventDefault();
+  if (!event.target.alt === "IMG") return;
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+instance.show();
+};
